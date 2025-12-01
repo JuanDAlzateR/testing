be43bf7a807aac36b9f97799db46a9a58ce26d99
JuanDAlzateR <219579616+JuanDAlzateR@users.noreply.github.com>
2025-12-01 16:30:24 -0500

    added weather rs.schema

--- src/main/java/com/solvd/testing/api/GetWeatherInfo.java
+++ src/main/java/com/solvd/testing/api/GetWeatherInfo.java
@@ -4,10 +4,12 @@ import com.zebrunner.carina.api.AbstractApiMethodV2;
 import com.zebrunner.carina.api.annotation.Endpoint;
 import com.zebrunner.carina.api.http.HttpMethodType;
 
-@Endpoint(url="https://www.meteosource.com/api/v1/free/",methodType = HttpMethodType.GET)
+
+// https://www.meteosource.com/api/v1/free/point?lat=2&lon=5&sections=current%2Chourly&language=en&units=auto&key=
+@Endpoint(url="https://www.meteosource.com/api/v1/free/point?",methodType = HttpMethodType.GET)
 public class GetWeatherInfo extends AbstractApiMethodV2 {
     public GetWeatherInfo(){
-        super("com/solvd/testing/weather/rq.json","com/solvd/testing/weather/rs.json","com/solvd/testing/weather/weather.properties");
+        super("api/weather/rq.json","api/weather/rs.json","com/solvd/testing/weather/weather.properties");
 //        replaceUrlPlaceholder("base_url", Configuration.getRequired("api_url"));
     }
 }
--- src/test/java/com/solvd/testing/MeteoTest.java
+++ src/test/java/com/solvd/testing/MeteoTest.java
@@ -1,7 +1,10 @@
 package com.solvd.testing;
 
 import com.solvd.testing.api.GetWeatherInfo;
+import com.solvd.testing.api.Place;
+import com.zebrunner.carina.api.http.HttpResponseStatusType;
 import com.zebrunner.carina.core.IAbstractTest;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
@@ -11,7 +14,15 @@ public class MeteoTest implements IAbstractTest {
 
     @Test()
     public void verifyGetWeatherInfoTest(){
-        GetWeatherInfo getWeatherInfo=new GetWeatherInfo();
+        Dotenv dotenv = Dotenv.load();
+        String apiKey = dotenv.get("API_KEY");
 
+        GetWeatherInfo getWeatherInfo=new GetWeatherInfo();
+        Place medellin =new Place("Medellin","6.265643","-75.574925");
+        getWeatherInfo.addProperty("place", medellin);
+        getWeatherInfo.addProperty("APIkey",apiKey);
+        getWeatherInfo.expectResponseStatus(HttpResponseStatusType.OK_200);
+        getWeatherInfo.callAPI(); //or callAPIExpectSuccess
+        getWeatherInfo.validateResponseAgainstSchema("api/weather/rs.schema"); //or validateResponse
     }
 }
